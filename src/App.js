cba1e5003e166048eb3199dcaad0060fdb836a66
Marc Johnson <marcjohnson@Marcs-MBP-2.home>
2022-08-08 09:25:56 +0100

    Init Commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Filecoin Green Storage Providers Reporting Portal!
+        
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://green.filecoin.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more about Filecoin Green
         </a>
       </header>
     </div>
