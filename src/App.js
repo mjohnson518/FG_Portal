063e31c70487b98a0665c2a3e8987b93221be826
Marc Johnson <marcjohnson@Marcs-MBP-2.home>
2022-08-09 15:53:22 +0100

    more tweaks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /> 
         <p>
           Welcome to the Filecoin Green Storage Providers Reporting Portal!
         </p>
