b5d99eefcdedc9aafb25e0d1c18f490ce02a37cd
Marc_Johnson <marcjohnson518@gmail.com>
2022-12-14 10:40:17 -0500

    express updt

--- src/express.js
+++ src/express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { Pool } = require('Elec');
+const { Pool } = require('PG');
 
 const app = express();
 
